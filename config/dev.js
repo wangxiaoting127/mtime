8690eb1e737ba41d622575482223d4741d3cf4e5
wxt <wxt@iMac.local>
2017-06-26 17:51:23 +0800

    unicode

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export const ID_PER=5
 export const MIN_MOVIE_ID=10000
-export const MAX_MOVIE_ID=201739
+export const MAX_MOVIE_ID=247241
 export const redisOpts = { }
 export const mongoUrl = "mongodb://localhost/mtime"
--- crawlers/movie.js
+++ crawlers/movie.js
@@ -1,72 +1,87 @@
 import * as Epona from "eponajs"
 import { korok } from "korok"
-import { trim, map, replace, compact,unescape } from "lodash"
+import { trim, map, replace, compact, unescape } from "lodash"
 
 export default async function (movieids) {
   let movieUrls = movieids.map(x => {
     return {
-      default: { _id: x, created_at: new Date }
+      default: { film_id: x, url: `http://movie.mtime.com/${x}/details.html`, created_at: new Date }
       , url: `http://movie.mtime.com/${x}/details.html`
     }
   })
   if (movieUrls.length == 1) { movieUrls = movieUrls[0] }
 
   let ret = await Epona.get(movieUrls, {
-    title: '.db_head h1 a::text()',
+    name: '.db_head h1 a::text()',
     make_company: {
       sels: '.wp49:nth-of-type(1) ul li *',
       nodes: {
-        _id: 'a::href|numbers',
-        cname: 'a::text()|trim',
-        country: 'span::text()|trim'
+        company_id: 'a::href|numbers',
+        company_name: 'a::text()|trim',
+        country: 'span::text()|trim',
+      },
+      filters: function (ret) {
+        let s = ret.map(x => {
+          x.country = x.country.replace(/&nbsp;/ig, '')
+          return x
+        })
+        return s
       }
     },
     release_company: {
       sels: '.wp49:nth-of-type(2) ul li *',
       nodes: {
-        _id: 'a::href|numbers',
-        cname: 'a::text()|trim',
+        company_id: 'a::href|numbers',
+        company_name: 'a::text()|trim',
         country: 'span::text()|trim'
+      },
+      filters: function (ret) {
+        let s = ret.map(x => {
+          x.country = x.country.replace(/&nbsp;/ig, '')
+          return x
+        })
+        return s
       }
     },
     cost: {
       sels: '.db_movieother_2 dd::$ ',
-      filters:function (x) {
+      filters: function (x) {
         if (x._root) {
-           let s=x._root.map(x => {
-            if (x.children[1].children[0].data === "制作成本：") {
-              let p = x.children.map(x => {
-                if (x.name === "p") {
-                  return x
-                }
-              })
-              // console.log(compact(p)[0].children[0].data)
-              return compact(p)[0].children[0].data
+          let s = x._root.map(x => {
+            if (x.children[1].children[0]) {
+              if (x.children[1].children[0].data === "制作成本：") {
+                let p = x.children.map(x => {
+                  if (x.name === "p") {
+                    return x
+                  }
+                })
+                // console.log(compact(p)[0].children[0].data)
+                return compact(p)[0].children[0].data
+              }
             }
-
           })
-          console.log(compact(s))   
           return compact(s)[0]
         }
       }
     },
     shooting_date: {
       sels: '.db_movieother_2 dd::$ ',
-      filters:function (x) {
+      filters: function (x) {
         if (x._root) {
-           let s=x._root.map(x => {
-            if (x.children[1].children[0].data === "拍摄日期：") {
-              let p = x.children.map(x => {
-                if (x.name === "p") {
-                  return x
-                }
-              })
-              // console.log(compact(p)[0].children[0].data)
-              return compact(p)[0].children[0].data
+          let s = x._root.map(x => {
+            if (x.children[1].children[0]) {
+              if (x.children[1].children[0].data === "拍摄日期：") {
+                let p = x.children.map(x => {
+                  if (x.name === "p") {
+                    return x
+                  }
+                })
+                // console.log(compact(p)[0].children[0].data)
+                let str = compact(p)[0].children[0].data
+                return str = str.replace(/&nbsp;/ig, '')
+              }
             }
-
           })
-          console.log(compact(s))   
           return compact(s)[0]
         }
       }
@@ -75,16 +90,6 @@ export default async function (movieids) {
       concurrent: 20
       // rateLimit: 1000
     })
-
-  // ret.pro_company=ret.pro_company.map(x=>{
-  //     let y=x.a+x.span
-  //    return  x=y.toString().replace('&nbsp;&nbsp;','')
-  // })
-  // ret.publisher=ret.publisher.map(x=>{
-  //     let y=x.a+x.span
-  //    return  x=y.toString().replace('&nbsp;&nbsp;','')
-  // })
-
 
   // console.log(ret)
   return ret
